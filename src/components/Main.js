1570e363b5b461a15cf027a149459093c72c6ae8
chrislyr0420 <li.yiran@wustl.edu>
2018-08-21 20:40:34 -0700

    shot-chart

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import nba from 'nba';
 import {Profile} from "./Profile";
 import {ShotChart} from "./ShotChart";
 
+
 export class Main extends React.Component {
+    componentDidMount() {
+        console.log(nba.findPlayer("Stephen Curry"));
+    }
+
     render() {
         return (
             <div className="main">
                 <Profile/>
-                <ShotChart/>
+                <ShotChart playerId={201939}/>
             </div>
         );
     }
