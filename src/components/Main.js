34206b0114360beec66846564c54573f89a0ee0c
chrislyr0420 <li.yiran@wustl.edu>
2018-08-21 20:50:50 -0700

    shot-chart

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,15 +5,28 @@ import {ShotChart} from "./ShotChart";
 
 
 export class Main extends React.Component {
+    state = {
+        playerId: nba.findPlayer("Chris Paul").playerId,
+        playerInfo: {}
+    }
+
     componentDidMount() {
-        console.log(nba.findPlayer("Stephen Curry"));
+        const playerId = nba.findPlayer("Chris Paul").playerId;
+        nba.stats.playerInfo({PlayerID: playerId}).then((response) => {
+            const playerInfo = Object.assign({}, response.commonPlayerInfo[0], response.playerHeadlineStats[0]
+            );
+            this.setState({
+                playerInfo
+            });
+        })
     }
 
     render() {
+        console.log(this.state.playerInfo);
         return (
             <div className="main">
                 <Profile/>
-                <ShotChart playerId={201939}/>
+                <ShotChart playerId={this.state.playerId}/>
             </div>
         );
     }
